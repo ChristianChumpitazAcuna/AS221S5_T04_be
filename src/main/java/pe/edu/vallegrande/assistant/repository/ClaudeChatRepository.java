4dec90e3adcc2568cc815e6d6be4b4e3f2cb3ce2
Christian <jesus.chumpitaz@vallegrande.edu.pe>
2024-05-19 11:30:04 -0500

    main branch cleaned

--- src/main/java/pe/edu/vallegrande/assistant/repository/ClaudeChatRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.assistant.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.vallegrande.assistant.model.ClaudeChat;
-
-@Repository
-public interface ClaudeChatRepository extends ReactiveCrudRepository<ClaudeChat, Long> {
-}
--- src/main/java/pe/edu/vallegrande/assistant/repository/GeminiChatRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.vallegrande.assistant.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.vallegrande.assistant.model.GeminiChat;
-
-@Repository
-public interface GeminiChatRepository extends ReactiveCrudRepository<GeminiChat, Long> {
-}
